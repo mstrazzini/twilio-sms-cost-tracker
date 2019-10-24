09ed8950ec8ca8d11df78bdaf30ef3e523b9ac5d
Marcos Trazzini <marcos@sypros.com.br>
2019-10-23 21:39:16 -0300

    setting up

---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const twilio = require('twilio')
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const bp = require('body-parser')
+const mongodb = require('mongodb')
+const libphonenumber = require('libphonenumber-js')
+
+const app = express()
+app.use(bp.urlencoded({ extended: false }))
+app.use(bp.json())
+
+let db = null
+
+const dbConnect = () => {
+  return new Promise((resolve, reject) => {
+    if (db) {
+      console.log('=> using cached db')
+      resolve()
+    } else {
+      mongodb.MongoClient.connect('mongodb://localhost/tracker', {
+        useUnifiedTopology: true
+      }).then(client => {
+          console.log('=> connected to the database')
+          db = client.db()
+          resolve()
+        }).catch(err => {
+          console.error('=> unable to connecto to db:', err.message ? err.message : JSON.stringify(err))
+          reject(err)
+        })
+    }
+  })
+}
+
+const priceMap = {
+  us: {
+    us: 0.0200,
+    br: 0.0570
+  }
+}
+
+const getCountryFromPhoneNumber = (phoneNumber) => {
+  return libphonenumber.parsePhoneNumber(phoneNumber).country
+}
+
+const getMessageCostData = (fromNumber, toNumber, numSegments) => {
+  const fromCountry = getCountryFromPhoneNumber(fromNumber).toLowerCase()
+  const toCountry = getCountryFromPhoneNumber(toNumber).toLowerCase()
+  const segmentCost = priceMap[fromCountry][toCountry]
+  const totalCost = segmentCost * numSegments
+
+  console.log(`FROM ${fromNumber} (${fromCountry}) TO ${toNumber} (${toCountry}) => ${totalCost}`)
+
+  return {
+    fromCountry, toCountry, segmentCost, totalCost
+  }
+}
+
+app.post('/sms', (req, res) => {
+  const { from, to, body } = req.body
+
+  console.log(JSON.stringify(req.body,null,2))
+
+  client.messages.create({
+    from, to, statusCallback: 'https://trazzini.ngrok.io/status', body
+  }).then(result => {
+    console.log(JSON.stringify(result,null,2))
+
+    const { sid, numSegments, dateCreated } = result
+
+    return dbConnect().then(() => {
+      return db.collection('log').insertOne({
+        _id: sid, numSegments, from, to, dateCreated
+      })
+    }).catch(console.error)
+
+  }).then(result => {
+    res.status(200).send(result)
+  }).catch(err => {
+    console.log('Error sending message:', err.message)
+    res.status(500).send({ messsage: err.message })
+  })
+})
+
+app.post('/status', (req, res) => {
+  console.log('STATUS UPDATE RECEIVED')
+  console.log(JSON.stringify(req.body,null,2))
+
+  const { SmsSid, SmsStatus} = req.body
+
+  if (SmsStatus == 'sent') {
+    dbConnect().then(() => {
+      return db.collection('log').findOne({ _id: SmsSid })
+    }).then(result => {
+      const { from, to, numSegments } = result
+      const { fromCountry, toCountry, segmentCost, totalCost } = 
+        getMessageCostData(from, to, numSegments)
+
+      return db.collection('log').updateOne({
+        _id: SmsSid
+      }, {
+        $set: {
+          fromCountry, toCountry, segmentCost, totalCost
+        }
+      })
+    }).then(() => {
+      res.status(201).send()
+    }).catch(console.error)
+  }
+
+})
+
+app.listen(3000, () => {
+  console.log('server is up and running on port 3000...')
+})
